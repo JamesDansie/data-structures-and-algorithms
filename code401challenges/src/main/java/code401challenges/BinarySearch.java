1add6eff09f4a2cdeb5d69fd1b886e9d80a20a32
JamesDansie <jdansie8336@gmail.com>
2019-09-18 18:33:45 -0700

    updated readme

--- code401challenges/src/main/java/code401challenges/BinarySearch.java
+++ code401challenges/src/main/java/code401challenges/BinarySearch.java
@@ -1,14 +1,15 @@
 package code401challenges;
 
 public class BinarySearch {
+    //this is very helpful; https://en.wikipedia.org/wiki/Binary_search_algorithm
     public static int binarySearch(int[] arrIn, int key){
         int lower = 0;
         int upper = arrIn.length - 1;
         while(lower <= upper){
             int mid = (lower + upper)/2;
 
-            String out = String.format("Lower is %d Upper is %d Mid is %d", lower, upper, mid);
-            System.out.println(out);
+//            String out = String.format("Lower is %d Upper is %d Mid is %d", lower, upper, mid);
+//            System.out.println(out);
 
             if(arrIn[mid] == key){
                 return mid;
